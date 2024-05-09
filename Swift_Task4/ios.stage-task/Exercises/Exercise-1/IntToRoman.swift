ce796e5b22cd969b1969ec305734fb6e31ecddf5
shpakusya <kate.shpakovskaya02@gmail.com>
2024-05-09 17:39:40 +0200

    Exercise 1 done

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -1,8 +1,23 @@
 import Foundation
 
 public extension Int {
-    
     var roman: String? {
-        nil
+        var romanString: String = ""
+        if (!(1...3999).contains(self)){ return nil }
+        let map: [(Int, String)] = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+        
+        var num = self
+        
+        while (num > 0){
+            for ex in map{
+                if(ex.0 <= num){
+                    num = num - ex.0
+                    romanString.append(contentsOf: ex.1)
+                    break
+                }
+            }
+        }
+        
+        return romanString
     }
 }
